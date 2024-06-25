77c8e62f9349fa13f017599d045d1321ff7224cd
曾尔文 <21373019@buaa.edu.cn>
2024-06-25 16:50:09 +0800

    id类型修改

--- CompanyManagement/models.py
+++ CompanyManagement/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -9,7 +11,7 @@ from shared.utils.datetime import get_expiry_time
 
 
 class Company(models.Model):
-    company_id = models.CharField(max_length=40, primary_key=True, auto_created=True)
+    company_id = models.UUIDField(primary_key=True, auto_created=True, unique=True, editable=False, default=uuid.uuid4)
     company_name = models.CharField(max_length=255)
     company_description = models.CharField(max_length=255)
     company_image = models.ImageField(upload_to='resources/company_images/', null=True, blank=True)
