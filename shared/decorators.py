80f0bb0c9423af2c9853af058633b5990f651920
曾尔文 <21373019@buaa.edu.cn>
2024-06-25 13:45:58 +0800

    添加企业相关的装饰函数

--- shared/decorators.py
+++ shared/decorators.py
@@ -4,6 +4,7 @@ from django.db.models import Q
 from django.http import JsonResponse
 from rest_framework import status
 
+from CompanyManagement.models import Company
 from UserManagement.models import *
 
 
@@ -32,4 +33,26 @@ def require_user(view_func):
         user = User.objects.get(query)
         request.user_object = user  # Attach user to request object so that it's available in the view
         return view_func(request, *args, **kwargs)
+
+    return _wrapped_view
+
+
+def require_company(view_func):
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        company_id = request.GET.get('company_id')
+        if not company_id:
+            company_id = request.data.get('company_id')
+
+        if not company_id:
+            return JsonResponse({'status': 'error', 'message': 'Missing company_id parameter'}, status=400)
+
+        try:
+            company = Company.objects.get(company_id=company_id)
+        except Company.DoesNotExist:
+            return JsonResponse({'status': 'error', 'message': 'Company does not exist'}, status=404)
+
+        request.company_object = company  # Attach company to request object so that it's available in the view
+        return view_func(request, *args, **kwargs)
+
     return _wrapped_view
