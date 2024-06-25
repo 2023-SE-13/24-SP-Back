bcf18474c130d901fbfff49bca4de6fd25eb907d
liataynat <1345334884@qq.com>
2024-06-25 15:55:08 +0800

    subscribe_firstversion

---
+++ Subscribe/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Subscribe_user
+from .models import Subscribe_company
+
+
+class SubscribeCompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscribe_company
+        fields = ['userid', 'companyid', 'createtime']
+
+    def create(self, validated_data):
+        # 如果createtime不由Django自动生成，你可以在这里设置它
+        # validated_data['createtime'] = ...
+        return Subscribe_company.objects.create(**validated_data)
+
+class SubscribeUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscribe_user
+        fields = ['user_src_id', 'user_dst_id', 'createtime']
+
+    def create(self, validated_data):
+        # 如果createtime不由Django自动生成，你可以在这里设置它
+        # validated_data['createtime'] = ...
+        return Subscribe_user.objects.create(**validated_data)
