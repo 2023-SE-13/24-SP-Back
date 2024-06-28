13c6fa639d859a1879077363d70669916afcfaae
曾尔文 <21373019@buaa.edu.cn>
2024-06-28 15:57:05 +0800

    调整动态相关的模型

--- TweetManagement/models.py
+++ TweetManagement/models.py
@@ -9,6 +9,8 @@ from UserManagement.models import User
 
 class Tweet(models.Model):
     tweet_id = models.UUIDField(primary_key=True, auto_created=True, unique=True, editable=False, default=uuid.uuid4)
+    is_retweet = models.BooleanField(default=False)
+    retweet_id = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -20,10 +22,22 @@ class Tweet(models.Model):
     def __str__(self):
         return f"{self.user} - {self.tweet_id}"
 
+class TweetPhoto(models.Model):
+    photo_id = models.UUIDField(primary_key=True, auto_created=True, unique=True, editable=False, default=uuid.uuid4)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='resources/tweet_photos/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        db_table = 'TweetPhotos'
+
+    def __str__(self):
+        return f"{self.tweet} - {self.photo_id}"
+
 class Comment(models.Model):
     comment_id = models.UUIDField(primary_key=True, auto_created=True, unique=True, editable=False, default=uuid.uuid4)
+    target_user = models.ForeignKey(User, on_delete=models.CASCADE)
     tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     class Meta:
