d72e264a9adb853cfcdba83537a88a7980e9fd20
Leonard <leonardzhou2077@gmail.com>
2024-06-28 17:24:56 +0800

    update

--- PositionManagement/urls.py
+++ PositionManagement/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from PositionManagement.views import create_position, get_position, delete_position, get_position_list
+from PositionManagement.views import *
 
 urlpatterns = [
     path('create_position', create_position),
     path('get_position', get_position),
     path('delete_position', delete_position),
-    path('get_position_list', get_position_list)
+    path('get_position_list', get_position_list),
+    path('apply_position', apply_position),
+    path('get_position_applications', get_position_applications),
 ]
