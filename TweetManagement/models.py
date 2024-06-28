c5561a773be33bd2d8731c4470ca61de87364f79
曾尔文 <21373019@buaa.edu.cn>
2024-06-28 11:42:57 +0800

    添加动态相关的模型

--- TweetManagement/models.py
+++ TweetManagement/models.py
@@ -12,10 +12,22 @@ class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    likes = models.IntegerField(default=0)
 
     class Meta:
         db_table = 'Tweets'
 
     def __str__(self):
         return f"{self.user} - {self.tweet_id}"
+
+class Comment(models.Model):
+    comment_id = models.UUIDField(primary_key=True, auto_created=True, unique=True, editable=False, default=uuid.uuid4)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        db_table = 'Comments'
+
+    def __str__(self):
+        return f"{self.user} - {self.comment_id} ({self.tweet})"
