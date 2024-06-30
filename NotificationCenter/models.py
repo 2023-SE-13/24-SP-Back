d248b4cd9938c63ca2f88f716eef23a5c37625c2
曾尔文 <21373019@buaa.edu.cn>
2024-06-30 17:01:33 +0800

    添加推送企业招聘信息

--- NotificationCenter/models.py
+++ NotificationCenter/models.py
@@ -3,6 +3,7 @@ import uuid
 from django.db import models
 
 from CompanyManagement.models import Company
+from PositionManagement.models import Position
 from TweetManagement.models import Tweet
 from UserManagement.models import User, Message
 
@@ -10,9 +11,9 @@ from UserManagement.models import User, Message
 # Create your models here.
 class Notification(models.Model):
     TYPE_CHOICES = (
-        ('message', 'Message Mention'),
-        ('subscribe', 'Subscribe Mention'),
-        ('system', 'System Notification')
+        ('message', 'Message Mention'), # chat message
+        ('subscribe', 'Subscribe Mention'), # tweet and position
+        ('system', 'System Notification') # offer
     )
     notification_id = models.UUIDField(primary_key=True, auto_created=True, unique=True, editable=False, default=uuid.uuid4)
     user = models.ForeignKey(User, related_name='notifications', on_delete=models.CASCADE)
@@ -23,3 +24,4 @@ class Notification(models.Model):
     tweet = models.ForeignKey(Tweet, related_name='notifications', on_delete=models.CASCADE, null=True)
     company = models.ForeignKey(Company, related_name='notifications', on_delete=models.CASCADE, null=True)
     message = models.ForeignKey(Message, related_name='notifications', on_delete=models.CASCADE, null=True)
+    position = models.ForeignKey(Position, related_name='notifications', on_delete=models.CASCADE, null=True)
