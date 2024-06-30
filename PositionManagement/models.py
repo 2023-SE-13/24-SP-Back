5bf1c93c2074f43be9623f1d6808a78b894a1af5
Leonard <leonardzhou2077@gmail.com>
2024-06-30 12:09:23 +0800

    create model

--- PositionManagement/models.py
+++ PositionManagement/models.py
@@ -6,6 +6,17 @@ from django.db import models
 from CompanyManagement.models import Company
 from UserManagement.models import User
 
+class PositionTag(models.Model):
+    category = models.CharField(max_length=255)
+    specialization = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'PositionTags'
+        unique_together = ('category', 'specialization')
+
+    def __str__(self):
+        return f"{self.category} - {self.specialization}"
+
 
 class Position(models.Model):
     position_id = models.UUIDField(primary_key=True, auto_created=True, unique=True, editable=False, default=uuid.uuid4)
@@ -18,7 +29,7 @@ class Position(models.Model):
     salary_max = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)  # 最高薪资
     posted_at = models.DateTimeField(null=True, blank=True)
     skill_required = models.ManyToManyField('UserManagement.Skill', blank=True)
-    position_tag = models.CharField(max_length=255, null=True, blank=True)
+    position_tag = models.ForeignKey(PositionTag, on_delete=models.CASCADE, null=True, blank=True)
  
     class Meta:
         db_table = 'Positions'
