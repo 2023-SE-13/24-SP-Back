54c615a5befa316aebde4e96568c360fc1c0ef14
曾尔文 <21373019@buaa.edu.cn>
2024-07-01 10:57:22 +0800

    change recommend

--- RecommendManagement/views.py
+++ RecommendManagement/views.py
@@ -19,7 +19,7 @@ from shared.decorators import require_position
 @api_view(['GET'])
 @authentication_classes([TokenAuthentication])
 def recommend_subscribe(request):
-    if request.user.username:
+    if not request.user.username:
         guest_recommends = {
             "users": [],
             "companies": []
